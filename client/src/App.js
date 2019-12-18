1be26deb77e4644e9fd644acac079cdb89352163
Jeffrey <jeffreymartespolanco@hotmail.com>
2019-12-17 17:06:10 -0800

    stretches 50% done

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 
 import SavedList from './Movies/SavedList';
-
+import MovieList from './Movies/MovieList'
+import Movie from './Movies/Movie'
+import {Switch,Route} from "react-router-dom"
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
 
@@ -10,10 +12,17 @@ const App = () => {
   };
 
   return (
+    
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Switch>
+      <Route exact path = '/'><MovieList/></Route>
+      <Route path = '/movies/:movieID'>
+        <Movie/> 
+        </Route>
+      </Switch>
     </div>
+    
   );
 };
 
